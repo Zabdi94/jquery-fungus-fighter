af30555c80b388d03f7d5f996fbfb9613371ef05
Zakariye Abdi <zak.abdi94@gmail.com>
2023-01-19 22:29:26 -0600

    Updated homework

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ $(document).ready(onReady);
 // Example:
 // let fungusHP = 100;
 
+let myHP = 100;
+let fungusHP = 100;
+
 function onReady() {
+    console.log ('ready to fight!')
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
+    $('attack-btn').click ('click', entangle) // lets you click it/ see this in the html
     
-    
+   
+
+
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
